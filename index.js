2de19a81f0debb9f2c455a0ca6aeeca57c9f7e1e
Eshimmel <90261234+Eshimmel@users.noreply.github.com>
2021-09-13 10:02:14 -0400

    yes

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 var router = require('express').Router();
 
 router.get("/", (req, res) => {
-  res.sendFile("/index.html");
+  res.sendFile("C:/Users/eshimmel/OneDrive - York County School of Technology/New/ExpressJS/index.html");
 });
 
 router.get('/about', function(req, res) {
-    res.sendFle('login');
+    res.sendFile("C:/Users/eshimmel/OneDrive - York County School of Technology/New/ExpressJS/about.html");
+});
+
+router.get('/about', function(req, res) {
+    res.sendFile("C:/Users/eshimmel/OneDrive - York County School of Technology/New/ExpressJS/ethan.html");
+});
+
+router.get('/about', function(req, res) {
+    res.sendFile("C:/Users/eshimmel/OneDrive - York County School of Technology/New/ExpressJS/tyler.html");
+});
+
+router.get('/about', function(req, res) {
+    res.sendFile("C:/Users/eshimmel/OneDrive - York County School of Technology/New/ExpressJS/justin.html");
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -3,5 +3,8 @@ const app = express();
 
 app.use('/', require('./index.js'));
 app.use('/about', require('./index.js'));
+app.use('/ethan', require('./index.js'));
+app.use('/', require('./index.js'));
+app.use('/about', require('./index.js'));
 
 app.listen('3000');
