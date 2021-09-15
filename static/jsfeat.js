bc8ef8c8415330396e9ec8299ef557c65553faf1
Eshimmel <90261234+Eshimmel@users.noreply.github.com>
2021-09-15 08:36:38 -0400

    Yes

--- static/jsfeat.js
+++ static/jsfeat.js
@@ -2,7 +2,9 @@ function showText() {
   var x = document.getElementById("btntest");
   if (x.style.display === "none") {
     x.style.display = "block";
+    x.name = "Show";
   } else {
     x.style.display = "none";
+    x.name = "Hide";
   }
 }
