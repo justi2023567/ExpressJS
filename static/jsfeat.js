8e81a87d775cf0ed87d6f41589f03257527ce5d6
Eshimmel <90261234+Eshimmel@users.noreply.github.com>
2021-09-15 09:33:26 -0400

    New

--- static/jsfeat.js
+++ static/jsfeat.js
@@ -2,17 +2,7 @@ function showText() {
   var x = document.getElementById("btnbox");
   if (x.style.display === "none") {
     x.style.display = "block";
-    toggleBtn();
   } else {
     x.style.display = "none";
-    toggleBtn();
-  }
-}
-function toggleBtn() {
-  var btn = document.getElementById("btn");
-  if (btn.value === "Hide") {
-    btn.value = "Show";
-  } else {
-    btn.value = "Show";
   }
 }
