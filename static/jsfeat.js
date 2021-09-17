fab4c32651ef8c6c1976344bf902cad395764e9c
Tsiegmund <Tyler2023411@live.ytech.edu>
2021-09-17 09:01:24 -0400

    w

--- static/jsfeat.js
+++ static/jsfeat.js
@@ -12,3 +12,19 @@ function showText() {
 function getRndInteger(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
   }
+
+  function startTime() {
+    const today = new Date();
+    let h = today.getHours();
+    let m = today.getMinutes();
+    let s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('txt').innerHTML =  h + ":" + m + ":" + s;
+    setTimeout(startTime, 1000);
+  }
+
+  function checkTime(i) {
+    if (i < 10) {i = "0" + i}; 
+    return i;
+  }
