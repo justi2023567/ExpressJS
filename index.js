b8fd3d0770fc4b4b13df128297bf0bc11415110d
Eshimmel <90261234+Eshimmel@users.noreply.github.com>
2021-09-21 10:11:02 -0400

    new

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var app = require('express');
 var router = app.Router();
 
+var names = {ethan: "Ethan Shimmel", tyler: "Tyler Siegmund", justin: "Justin Anderson"}
+
 router.get("/", (req, res) => {
   res.render("index");
 });
@@ -10,11 +12,14 @@ router.get('/about', function(req, res) {
 });
 
 router.get('/ethan', function(req, res) {
-    res.render("ethan");
+    res.render("ethan", {name: names.ethan});
 });
 
 router.get('/tyler', function(req, res) {
-    res.render("tyler");
+    res.render("ethan", {
+      name: names.tyler
+
+    });
 });
 
 router.get('/justin', function(req, res) {
