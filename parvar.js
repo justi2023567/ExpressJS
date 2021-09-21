1560a883265bf8d60ca7d47a35f06a43a088195b
Tsiegmund <Tyler2023411@live.ytech.edu>
2021-09-21 10:16:23 -0400

    yes

---
+++ parvar.js
@@ -0,0 +1,5 @@
+var ethanpara = "I am a hard working student with a 4.0 GPA. I play basketball and baseball here at York County School of Technology.
+I am able to adapt to many different work environments. I would prefer to work as a game developer but look to go to college first
+I want to be able to build my own game and create a company from it."
+
+module.exports = parvar;
