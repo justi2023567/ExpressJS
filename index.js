31878b35dccb0e4638040b825647f5387ecb4087
Eshimmel <90261234+Eshimmel@users.noreply.github.com>
2021-09-21 10:34:16 -0400

    new

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var app = require('express');
 var router = app.Router();
+const ethanpara = require('./parvar.js');
 
 var names = {ethan: "Ethan Shimmel", tyler: "Tyler Siegmund", justin: "Justin Anderson"}
 
@@ -12,7 +13,10 @@ router.get('/about', function(req, res) {
 });
 
 router.get('/ethan', function(req, res) {
-    res.render("ethan", {name: names.ethan});
+    res.render("ethan", {
+      name: names.ethan,
+      paragraph: ethanpara
+    });
 });
 
 router.get('/tyler', function(req, res) {
--- parvar.js
+++ parvar.js
@@ -1,5 +1,7 @@
-var ethanpara = "I am a hard working student with a 4.0 GPA. I play basketball and baseball here at York County School of Technology.
-I am able to adapt to many different work environments. I would prefer to work as a game developer but look to go to college first
-I want to be able to build my own game and create a company from it."
+var ethanpara = "I am a hard working student with a 4.0 GPA.\n" +
+" I play basketball and baseball here at York County School of Technology." +
+" I am able to adapt to many different work environments." +
+" I would prefer to work as a game developer but look to go to college first." +
+" I want to be able to build my own game and create a company from it."
 
-module.exports = parvar;
+module.exports = ethanpara;
