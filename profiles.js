21732c9ef988605fd929f5781791ad27a6645720
justi2023567 <82458643+justi2023567@users.noreply.github.com>
2021-10-06 13:52:32 -0400

    Create profiles.js

---
+++ profiles.js
@@ -0,0 +1,18 @@
+}
+  var justin = {
+    name : "Justin",
+    bio : "<p>I\'Im dumb",
+    title : "Professional freeloader"
+
+    var tyler = {
+      name : "Tyler",
+      bio : "<p>I\'Im dumb",
+      title : "Professional freeloader"
+
+      var ethan = {
+        name : "Ethan",
+        bio : "<p>I\'Im dumb",
+        title : "Professional freeloader",
+  }
+ }
+}
